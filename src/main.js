cfdcaf58cb889e5e1a2dbeeab2fc24a3595517f3
els2003 <eullerlou321@gmail.com>
2025-04-10 17:58:30 -0300

    Migração do tema Modernize

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+import App from './App.vue';
+import router from './router';
+import { createApp } from 'vue';
+import vuetify from './plugins/vuetify';
+import '@/scss/style.scss';
 
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
-import { createApp } from 'vue'
-
-const app = createApp(App)
-
-registerPlugins(app)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router).use(vuetify).mount("#app");
